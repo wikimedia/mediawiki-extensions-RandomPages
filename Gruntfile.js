e9f51ab21c046eeb9f57b1eb3698673d184c3caf
zoranzoki21 <zorandori4444@gmail.com>
2018-04-15 11:36:42 +0200

    Add Jenkins build support
    Change-Id: If21cd605771c845324175e7873cfeb8b74e1bf46

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	grunt.initConfig( {
+
+		jshint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'jshint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
