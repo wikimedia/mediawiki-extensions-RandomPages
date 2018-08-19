88297328b0f5003c0c5942c2f5245092eba183d2
Zoranzoki21 <zorandori4444@gmail.com>
2018-08-19 09:06:51 +0000

    Fix typos in code
    Change-Id: I24bb91627dcc1d98f15736c55f04bd1512dc108c

--- RandomPages.hooks.php
+++ RandomPages.hooks.php
@@ -4,7 +4,7 @@ class RandomPagesHooks {
 	/**
 	 * Setups the mediawiki hook
 	 */
-	static function aoRandomPages() {
+	  static function aoRandomPages() {
 	  global $wgParser;
 	  $wgParser->setHook('randompages', [ __class__ , 'aoRandomPagesHook']);
 	}
@@ -21,7 +21,7 @@ class RandomPagesHooks {
 	 * Sample Usage:
 	 *
 	 * <code>
-	 *  <randompages limit="10" namespace="true" levels="10" />
+	 * <randompages limit="10" namespace="true" levels="10" />
 	 * </code>
 	 *
 	 * Gets 10 random pages from the global namespace with 10 levels of style
@@ -30,30 +30,28 @@ class RandomPagesHooks {
 	 * @param $params array, additional parameters passed as attributes to randompages tag
 	 * @param $parser Parser, the Wiki Parser Object
 	 */
-	static function aoRandomPagesHook($text, $params, $parser) {
+	  static function aoRandomPagesHook($text, $params, $parser) {
 	  global $wgDBprefix;
-	  // prevent caching for this wiki page
+	  // Prevent caching for this wiki page
 	  $parser->disableCache();
-	  // get parameters
+	  // Get parameters
 	  $limit = isset($params['limit']) ? (int)$params['limit'] : 150;
 	  $namespaced = isset($params['namespace']) ? $params['namespace'] == 'true' : false;
 	  $levels = isset($params['levels']) ? (int)$params['levels'] : 5;
-	  // build sql query
+	  // Build sql query
 	  $sql = sprintf('select * from %spage where', $wgDBprefix);
 	  $sql .= $namespaced ? ' page_namespace = 0 ' : ' 1=1 ';
 	  $sql .= sprintf('order by rand() limit %d', $limit);
-	  // execute that.
+	  // Execute that.
 	  $dbr = wfGetDB( DB_SLAVE );
 	  $rs  = $dbr->query( $sql );
 	  $buff = '<div class="randomPages">';
 	  while( $row = $rs->fetchObject( $rs ) ) {
 		$buff .= '<span class="randomPages_level'.rand(1,$levels).'">';
 		$title = Title::makeTitleSafe($row->page_namespace, $row->page_title);
-		// manually call __toString for php < 5.2
 		$buff .= sprintf('<a href="%s" title="%s">%s</a><br>', $title->getLocalURL(), $title->__toString(), $title->__toString());
-		#$buff .= sprintf('<a href="%s" title="%s">%s</a>', $title->escapeLocalURL(), $title->__toString(), $title->__toString());
 		$buff .= '</span>';
-	  }
-	  return $buff . '</div>';
+	}
+		return $buff . '</div>';
 	}
 }
